9724a4a8abfea56e915225c28a77ba092f18422e
jimmy <gjinming@wisec.org.cn>
2018-03-19 17:29:10 +0800

    add rc4 encrypt algorithm

---
+++ rc4/rc4.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <string.h>
+
+void rc4_init(unsigned char *s, unsigned char *key, unsigned long len)
+{
+	int i = 0, j = 0;
+	char k[256] = {0};
+	unsigned char tmp = 0;
+
+	for (i = 0; i < 256; i++)
+	{
+		s[i] = i;
+		k[i] = key[i%len];
+	}
+
+	for (i = 0; i < 256; i++)
+	{
+		j = (j + s[i] + k[i]) % 256;
+		tmp = s[i];
+		s[i] = s[j]; // exchange s[i] and s[j]
+		s[j] = tmp;
+    }
+}
+
+/* encrypt and decrypt function */
+void rc4_crypt(unsigned char *s, unsigned char *Data, unsigned long len)
+{
+	int i = 0, j = 0, t = 0;
+	unsigned long k = 0;
+	unsigned char tmp;
+
+	for (k = 0; k < len; k++)
+	{
+		i = (i + 1) % 256;
+		j = (j + s[i]) % 256;
+		tmp = s[i];
+		s[i] = s[j]; // exchange s[i] and s[j]
+		s[j] = tmp;
+		t = (s[i] + s[j]) % 256;
+		Data[k] ^= s[t];
+    }
+}
+
+int main(int argc, char *argv[])
+{
+	unsigned char s[256] = {0}, s2[256] = {0}; // S-box
+	char key[256] = "justfortest";
+	char pData[512] = "helloworld";
+	unsigned long len = strlen(pData);
+	int i;
+
+	printf("pData = %s\n", pData);
+	printf("key = %s, length = %d\n\n", key, strlen(key));
+	rc4_init(s, (unsigned char *)key, strlen(key)); // 初始化
+	printf("完成对 S[i] 的初始化，如下：\n\n");
+	for (i = 0; i < 256; i++)
+	{
+		printf("%d", s[i]);
+		if (i && (i + 1) % 16 == 0)
+		{
+			putchar('\n');
+		}
+    }
+	printf("\n\n");
+	for (i = 0; i < 256; i++) // 用 s2[i] 暂时保留经过初始化的 s[i]，很重要的！！！
+	{
+		s2[i] = s[i];
+	}
+	printf("已经初始化，现在加密:\n\n");
+	rc4_crypt(s, (unsigned char *)pData, len); // 加密
+	printf("pData = %s\n\n", pData);
+	printf("\n已经加密，现在解密:\n\n");
+	rc4_crypt(s2, (unsigned char *)pData, len); // 解密
+	printf("pData = %s\n\n", pData);
+
+	return 0;
+}
+
